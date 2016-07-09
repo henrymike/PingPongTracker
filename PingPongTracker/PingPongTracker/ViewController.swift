ac99b5190a05aa4bce9c1db4b73f074950377936
Mike Henry <chsmike@gmail.com>
2016-07-09 16:55:24 -0400

    Basic UI elements wired with accessibility labels in storyboard; simple incrementing for scoring

--- PingPongTracker/PingPongTracker/ViewController.swift
+++ PingPongTracker/PingPongTracker/ViewController.swift
@@ -9,15 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var playerOneNameLabel :UILabel!
+    @IBOutlet weak var playerTwoNameLabel :UILabel!
+    @IBOutlet weak var playerOneScoreLabel :UILabel!
+    @IBOutlet weak var playerTwoScoreLabel :UILabel!
+    
+    
+    //MARK: Scoring
+    
+    var playerOneScore : Int = 0
+    var playerTwoScore : Int = 0
+    
+    //MARK: Interactivity Methods
+    
+    @IBAction func settingsButtonPressed(sender: UIBarButtonItem) {
+        //settings VC segue
+    }
+    
+    @IBAction func playerOneIncrementUpButtonPressed(sender: UIButton) {
+        playerOneScore = playerOneScore+1
+        playerOneScoreLabel.text = String(playerOneScore)
+        print("P1: ",playerOneScore)
+    }
+    
+    @IBAction func playerOneIncrementDownButtonPressed(sender: UIButton) {
+        playerOneScore = playerOneScore-1
+        playerOneScoreLabel.text = String(playerOneScore)
+        print("P1: ",playerOneScore)
+    }
+    
+    @IBAction func playerTwoIncrementUpButtonPressed(sender: UIButton) {
+        playerTwoScore = playerTwoScore+1
+        playerTwoScoreLabel.text = String(playerTwoScore)
+        print("P2: ",playerTwoScore)
+    }
+    
+    @IBAction func playerTwoIncrementDownButtonPressed(sender: UIButton) {
+        playerTwoScore = playerTwoScore-1
+        playerTwoScoreLabel.text = String(playerTwoScore)
+        print("P2: ",playerTwoScore)
+    }
+    
+    //MARK: Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 
